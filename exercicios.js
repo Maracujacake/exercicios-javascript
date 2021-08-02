c1f90f231ed1d788698d3b482a519d41853583f3
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-02 13:04:23 -0300

    3

--- exercicios.js
+++ exercicios.js
@@ -33,3 +33,59 @@ function valores(valor1, valor2){
 }
 
 /**************************************************************************************/
+
+// exercício 3 - FizzBuzz
+
+// caso não seja um número, retorne erro
+// caso divísivel por 3, retorne fizz
+// caso divisível por 5, retorne buzz
+// caso divisível por 3 e 5, retorne FizzBuzz
+// caso não seja divisível por nenhum deles, retorne o próprio valor
+
+const resultado = FizzBuzz()
+console.log(resultado)
+
+function FizzBuzz(entrada){
+    if(typeof entrada !== 'number')
+    return 'Erro! Não é um número'
+
+    if(entrada % 3 === 0 && entrada % 5 === 0)
+    return 'FizzBuzz'
+
+    if(entrada % 3 === 0)
+    return 'Fizz'
+
+    if(entrada % 5 === 0)
+    return 'Buzz'
+
+    return entrada
+}
+
+/**************************************************************************************/
+
+// verificador de velocidade
+// velocidade maxima = 70km
+// ao passar de 70km, a cada 5km receberá um ponto na carteira
+// ao passar de 12 pontos, a carteira será suspensa
+// uso do Math.floor para arredondar valores
+// criação de consts para refatorar o código
+
+verificarVelocidade();
+
+function verificarVelocidade(velocidade){
+const VelocidadeMaximum = 70;
+const PontosACada = 5;
+const PontosMaximum = 12;
+    if(velocidade <= VelocidadeMaximum)
+    console.log('Ok');
+    
+    else{
+        const pontos = Math.floor(((velocidade - VelocidadeMaximum)/PontosACada));
+        if(pontos >= PontosMaximum)
+            console.log('Carteira suspensa')
+        else 
+            console.log('Pontos: '+pontos);
+    }
+}
+
+/**************************************************************************************/
