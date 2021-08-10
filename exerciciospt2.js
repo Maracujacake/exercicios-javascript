016dc4d8d96758ad05a1943646a961294490d02b
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-08-10 13:47:00 -0300

    1

--- exerciciospt2.js
+++ exerciciospt2.js
@@ -12,30 +12,3 @@
     // indique o indice, o que quer excluir e o valor
     console.log(numerosaleatorios);
 
-    /**************************************************************************************/
-
-    // verificação se há algum elemento dentro de um array
-
-    const muitoselementos = ['abc',5,8,'showdebola','chocolate'];
-    muitoselementos.indexOf(5); // vai mostrar em qual índice está o 5 SE ele existir dentro do array
-    // caso o elemento não existe, vai retornar -1;
-    muitoselementos.lastIndexOf(8); // retorna o índice da ULTIMA APARIÇÃO do elemento
-
-    muitoselementos.includes('abc'); // retorna um true se existir dentro do array e um false caso não exista
-
-    /**************************************************************************************/
-
-    // Achar elementos dentro de um array de objetos ou dentro de um objeto 
-
-    const carros = [
-        {marca: 'wolksvagen', motor: '1.6v'},
-        {marca: 'peugeot', motor: '2.0v'},
-        {marca: 'ferrari', motor: '2.5v'}
-    ];
-
-    const acharCarros = carros.find(function(Carros){
-        return Carros.marca === 'wolksvagen'
-    });
-    console.log(acharCarros);
-
-    /**************************************************************************************/
